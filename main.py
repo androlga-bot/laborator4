f64004ab10bed310274dd76dd5bc5be49953b04e
Ширяев Антон <70949517+androlga-bot@users.noreply.github.com>
2020-12-21 21:50:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import lab4
 
-def Entity(player1):
+def entity(player1):
     print(player1.get_name(), '\nмана: ', player1.get_mana(), '\nжизни: ',
       player1.get_life(), '\nпозиция: ', player1.get_pos(),'\n')
 
     
 player1=lab4.Player('Кирсан Кайфат',200)
 player2=lab4.Mage('Олег',100)
-Entity(player1)
-Entity(player2)
+entity(player1)
+entity(player2)
 Roshag=lab4.Mob('Рошаг',190)
 Kali=lab4.Mob('Кали',290)
 Roshag.attack(player1)
 player1.heal(100)
 player1.attack(Roshag,100)
-Entity(Roshag)
-Entity(player1)
+entity(Roshag)
+entity(player1)
 player1.move(5)
-Entity(player1)
+entity(player1)
