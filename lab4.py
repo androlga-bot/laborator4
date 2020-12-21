7052b64f3e87e6e8cb2aa4e3063c6ff2a67e03c1
Ширяев Антон <70949517+androlga-bot@users.noreply.github.com>
2020-12-21 21:49:34 +0300

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -1,12 +1,12 @@
-class Entity:
+class entity:
     name = 'entity'    
     level = 1
-    mana_Equiv = 10 * level
-    regen_Mana = mana_Equiv / 4
-    life_Equiv = 10 * level
-    mana = mana_Equiv
-    life = life_Equiv
-    regen_Life = life_Equiv / 5
+    mana_equiv = 10 * level
+    regen_mana = mana_equiv / 4
+    life_equiv = 10 * level
+    mana = mana_equiv
+    life = life_equiv
+    regen_life = life_equiv / 5
     position = 0
     damage = 2 * level
     status = 'Alive'
@@ -30,14 +30,14 @@ class Entity:
 
     def move(self, direct):
         self.position += direct
-        if self.mana + self.regen_Mana <= self.mana_Equiv:
-            self.mana += self.regen_Mana
+        if self.mana + self.regen_mana <= self.mana_equiv:
+            self.mana += self.regen_mana
         else:
-            self.mana = self.mana_Equiv
-        if self.life + self.regen_Life <= self.life_Equiv:
-            self.life += self.regen_Life
+            self.mana = self.mana_equiv
+        if self.life + self.regen_life <= self.life_equiv:
+            self.life += self.regen_life
         else:
-            self.life = self.life_Equiv
+            self.life = self.life_equiv
 
     def  heal(self, mana):
         if self.mana - mana >= 0:
@@ -45,10 +45,10 @@ class Entity:
         else:
             mana = self.mana
             self.mana = 0
-        if self.life+mana <= self.life_Equiv:
+        if self.life + mana <= self.life_equiv:
             self.life += mana
         else:
-            self.life = self.life_Equiv
+            self.life = self.life_equiv
 
 
     def attack(self, mob, mana = 0):
@@ -67,12 +67,12 @@ class Player(Entity):
         super().__init__()
         self.name = name
         self.level = level
-        self.mana_Equiv = 15 * level
-        self.regen_Mana = self.mana_Equiv / 4
-        self.life_Equiv = 15 * level
-        self.mana = self.mana_Equiv
-        self.life = self.life_Equiv
-        self.regen_Life = self.life_Equiv / 4
+        self.mana_equiv = 15 * level
+        self.regen_mana = self.mana_equiv / 4
+        self.life_equiv = 15 * level
+        self.mana = self.mana_equiv
+        self.life = self.life_equiv
+        self.regen_life = self.life_equiv / 4
         self.damage = 3 * level
 
 
@@ -81,12 +81,12 @@ class Mage(Entity):
         super().__init__()
         self.name = name
         self.level = level
-        self.mana_Equiv = 30 * level
-        self.regen_Mana = self.mana_Equiv / 2
-        self.life_Equiv = 10 * level
-        self.mana = self.mana_Equiv
-        self.life = self.life_Equiv
-        self.regen_Life = self.life_Equiv / 5
+        self.mana_equiv = 30 * level
+        self.regen_mana = self.mana_equiv / 2
+        self.life_equiv = 10 * level
+        self.mana = self.mana_equiv
+        self.life = self.life_equiv
+        self.regen_life = self.life_equiv / 5
         self.damage = 2 * level
 
 
@@ -95,12 +95,12 @@ class Warrior(Entity):
         super().__init__()
         self.name = name
         self.level = level
-        self.mana_Equiv = 10 * level
-        self.regen_Mana = self.mana_Equiv / 5
-        self.life_Equiv = 30 * level
-        self.mana = self.mana_Equiv
-        self.life = self.life_Equiv
-        self.regen_Life = self.life_Equiv / 2
+        self.mana_equiv = 10 * level
+        self.regen_mana = self.mana_equiv / 5
+        self.life_equiv = 30 * level
+        self.mana = self.mana_equiv
+        self.life = self.life_equiv
+        self.regen_life = self.life_equiv / 2
         self.damage = 4 * level
 
         
@@ -110,8 +110,8 @@ class Mob(Entity):
         super().__init__()
         self.name = name
         self.level = level
-        self.mana_Equiv = 0
+        self.mana_equiv = 0
         self.damage = 6 * level
-        self.life_Equiv = 10*level
-        self.life = self.life_Equiv
-        self.regen_Life = self.life_Equiv/5
+        self.life_equiv = 10 * level
+        self.life = self.life_equiv
+        self.regen_life = self.life_equiv/5
